a5953cf7be78dfcde08a97bd280d5cdc245cc22f
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-06-05 15:38:46 +0300

    4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './css/styles.css';
 import { makeCardsMarkup } from './js/markup';
 import ApiService from "./js/api-service";
 import LoadMoreBtn from './js/load-more-btn';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const lightbox = new SimpleLightbox('.gallery a');
 
 const refs = {
   // searchForm: document.querySelector(".search-form"),
@@ -10,8 +16,7 @@ const refs = {
 };
  const searchForm = new LoadMoreBtn({
    selector: '.search-form',
-   
- });
+    });
 searchForm.showForm();
 
  const loadMoreBtn = new LoadMoreBtn({
@@ -19,38 +24,55 @@ searchForm.showForm();
    hidden: true,
  });
 const newsApiService = new ApiService();
-console.log(newsApiService);
-console.log(loadMoreBtn);
-console.log(searchForm);
+// console.log(newsApiService);
+// console.log(loadMoreBtn);
+// console.log(searchForm);
 searchForm.refs.button.addEventListener("submit", onSearch);
-loadMoreBtn.refs.button.addEventListener("click", onLoadMore);
-
+loadMoreBtn.refs.button.addEventListener("click", fetchHits);
 
 
 function onSearch(e) {
   e.preventDefault();
     newsApiService.query = e.currentTarget.elements.searchQuery.value;
   if (newsApiService.query === '') {
-    return alert;
+    return Notify.failure(`The search string cannot be empty. Please specify your search query`);
   }
   
   loadMoreBtn.show();
   newsApiService.resetPage();
   clearCardsContainer();
-   console.dir(newsApiService.query);
+  console.dir(newsApiService.query);
   searchForm.hideForm();
   fetchHits();
   
 };
 
-function onLoadMore() {
-  fetchHits();
-};
+// function onLoadMore() {
+//   fetchHits();
+// };
 
 function fetchHits() {
   loadMoreBtn.disable();
-  newsApiService.fetchArticles().then(hits => {
-    appendCardsMarkup(hits);
+  newsApiService.fetchArticles().then(data => {
+    console.log(data.hits, data.totalHits)
+    if (data.hits. length === 0) {
+            Notify.failure(`Sorry, there are no images matching your search query: ${newsApiService.query}. Please try again.`);
+            loadMoreBtn.hide();searchForm.showForm();
+            return;
+        }
+      
+    if (newsApiService.page === 2) {
+      console.log("поточна сторынка",newsApiService.page);
+       Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    };
+    appendCardsMarkup(data.hits);
+     lightbox.refresh();
+    if (newsApiService.page > Math.round(data.totalHits / data.hits.length)) {
+      console.log(Math.round(data.totalHits / data.hits.length))
+      Notify.failure(`We're sorry, but you've reached the end of search results.`);
+      loadMoreBtn.hide();
+    return;
+    };
     loadMoreBtn.enable();
     searchForm.showForm();
   });
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -5,19 +5,33 @@ export default class ApiService{
     this.searchQuery = '';
     this.page = 1;
   }
-  fetchArticles() {
-    console.log("до", this);
-    return fetch(`${BASE_URL}?key=27772870-4058b108341efce898c1dbbbe&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
-      .then(response => response.json())
-      .then(({hits}) => {
-        this.page += 1;
-       console.log(hits);
-        console.log("after", this);
-        return hits;
-      });
-  }
+  // fetchArticles() {
+  //   console.log("до", this);
+  //   return fetch(`${BASE_URL}?key=27772870-4058b108341efce898c1dbbbe&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //     this.page += 1;
+  //     console.log(data);
+  //       console.log(data.hits, data.total, data.totalHits);
+       
+  //       console.log("after", this);
+  //       return data;
+  //     });
+  // }
+
+  async fetchArticles() {
+    try {
+      const responce = await axios.get(`${BASE_URL}?key=27772870-4058b108341efce898c1dbbbe&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+    }
+    
+  
   resetPage(){
     this.page = 1;    
+  }
+  firstFetch() {
+    if (this.page = 2) {
+       Notify.info(`Hooray! We found ${totalHits} images.`);
+    }
   }
   get query() {
     return this.searchQuery;
